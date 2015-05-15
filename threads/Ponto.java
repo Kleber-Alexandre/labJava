0d1e1aa9e841b02262a1ce4a67c875bd58f0d05b
Kleber-Alexandre <k.alex.alves@gmail.com>
2015-05-15 15:11:28 -0300

    pasta que contem as threads

---
+++ threads/Ponto.java
@@ -0,0 +1,25 @@
+package threads;
+
+import java.lang.Thread;
+
+
+public class Ponto implements Runnable
+
+{
+	public void run()
+	{
+	  try {
+			for (int i=0; i<10; i++)
+	        {
+	
+				System.out.print (".");
+				Thread.sleep(2000);
+
+			}
+
+	      }
+		catch (InterruptedException e)
+		{
+		}
+	}
+}
